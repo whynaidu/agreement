ff9e6a7a4e65546f40a6d12bbd57ab21570e6718
whynaidu <naiduvedant@gmail.com>
2022-04-20 10:33:19 +0530

    added  agreement

---
+++ agreement.php
@@ -0,0 +1,156 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Police NOC</title>
+</head>
+<style>
+	table , td, th {
+	border: 1px solid #595959;
+	border-collapse: collapse;
+}
+td, th {
+	padding: 3px;
+	width: 30px;
+	height: 25px;
+}
+th {
+	background: #f0e6cc;
+}
+.even {
+	background: #fbf8f0;
+}
+.odd {
+	background: #fefcf9;
+}
+.receipt{
+	text-align: center;
+}
+	
+	
+</style>
+<body>
+
+<p><b>WHERE AS : </b>       </p>                                                                                                                                                                                     <p>The LICENSOR is fully seized and possessed of or otherwise well and sufficiently entitled to hold the following  FLAT/SHOP:</p> <br>
+			
+<div>
+	<table style="width: 100%;">
+	<tbody>
+		<tr>
+			<td>FLAT/SHOP NO.</td>
+			<td>PLOT NO.</td>
+			<td>SECTOR</td>
+			<td>AREA(in Sq.feet)</td>
+		</tr>
+		<tr>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+		</tr>
+		<tr>
+			<td colspan="4">CIDCO APARTMENT:</td>
+		</tr>
+		<tr>
+			<td colspan="4">CO.OP.HSG.SOCITY:</td>
+		</tr>
+		<tr>
+			<td colspan="4">NODE:</td>
+		</tr>
+	</tbody>
+</table>
+</div><br>
+ (hereinafter called and referred to as ‘‘ THE SAID FLAT / SHOP).<br>
+<p><b>AND WHERE AS:</b></p>
+<p>The owner who on account of certain personal reasons is not occupying the said premise; and the LICENSEE being temporarily in need of a RESIDENTIAL ACCOMODATION/BUSINESS PREMISES requested the owner to give  on ‘‘LEAVE & LICENSE BASIS , as a temporary facility, the use of the said premises, together with the fixtures and lying thereon, on the terms and conditions recorded hereinafter.</p>
+<p><b>AND WHERE AS:</b></p>
+<p>As per the recent orders from the Department of police,  the Licensee furnish the following details and further agrees to co-operate in getting No Objection Certificate from the local police by appearing personally as when called by the police under which jurisdiction the said premise is covered.</p>
+<p>i)	Permanent Native Address:</p>
+      
+<p>ii)	Present Address :</p>
+
+<p>iii) The photograph of the LICENSOR and LICENSEE is a appended in appropriate place.</p>
+
+	<p><b>OWN THIS AGREEMENT WITNESSETH AS UNDER</b></p>
+<p>1.	The owner do hereby grants to the LICENSEE his/her permission to enter upon, occupy and look after, temporarily, the said premises for a certain period of        MONTHS,  which shall commence from DAY of ___________________. And shall expire on this day of _________________</p>
+<p>2.	The LICENSEE convents with the owenr that LICENSEE  shall observe and perform the following terms and conditions:</p>
+<p>b)     To pay a Monthly compensation of sum of Rs. _______________________/-  (RUPEES
+___________________________________________ONLY.)  as per English calendar month, in advance and thereafter on the 10th of each ensuring month.</p>
+<p>C)     The Electricity, water and any others applicable service charges shall be regularly paid by the LICENSEE, where                                      society maintenance charges and Lease Tax , property TAX , if any shall be paid by the OWNER/LICENSOR.</p>
+<p>d)     To use the said premise for RESIDENTIAL/BUSINESS purpose only . Note to cause ,permit or suffer anything in any                          way which may become a nuisance  or annoyance or cause damage/ loss to the said premises or to the neighbor ‘s property.</p>
+<p>e)     Not to sublet, transfer or otherwise part with the possession of the said premises or any part thereof to anyone.</p>
+<p>f)      TO permit the owner and facilitate him/her to inspect the said premises at any reasonable time during the period of this Agreement.</p>
+<p>3.      It is hereby expressly agreed and declared that neither this  Agreement nor anything contained herein shall be deemed to create any interest or estate in favor of the LICENSEE in the said premise and that relationship of the two parties shall always be that of LICENSOR and LICENSEE only.</p>
+<p>4) The LICENSEE  is entitled to surrender the said premises by giving the LICENSOR, one calendar month’s notice, in writing  or his intention and at the expiry of the said notice period, this Agreement shall REVOKED and the LICENSEE shall be entitled to get the security Deposit amount refunded, against the delivery of the vacant possession.</p>
+<p>a) The LICENSOR may also give one calendar month’s  notice, in case the LICENSOR wish to terminate the Agreement entered in to between the parties, and is ready and willing to refund the security  deposit against delivery of the possession.</p>
+<p>5. on expiry of the Agreement period , the LICENSEE shall remove all his belongings and shall handover vacant and peaceful  physical possession back to the LICENSOR and shall quit the the said premise without causing hindrance . The LICENSEE is liable to pay for any damages caused by him/her, And the LICENSOR Shall give back the security.</p>
+<p>6. In case the LICENSEE does not vacate the said premises on the expiry of the term , then owner shall be entitled to and he is hereby authorized by the LICENSEE to remove all the LICENSEE’S belongings and keep the same outside the said premises without being responsible for any loss or damage caused to the same thereto.</p>
+<p>7.    Without prior NOC of the Flat/Shop Owner, the Licensee CAN NOT apply for RATION CARD, GAS CONNECTION, MOBILE or LANDLINE,CREDIT CARD, ANY KIND OF LOANS. If so done by the Licensee shall be responsible and liable for the bill pending/outstanding of any mobile company, banks or financial institution. The flat/shop is given on purely temporary basis to the Licensee only for RESIDENTIAL/BUSINESS purpose only.</p>
+<p>8.     Control Act 1999, as amended up to date, whereby it is mentioned if the licensee fails to deliver possession of the licensee period , the licensee shall be liable to pay damages/compensation, provided under this agreement form the date of such failure to the actual date of handling over possession of the  said premise.</p>
+<p>9. The following persons are staying with the Licensee :</p>
+                       
+<table style="width: 100%;">
+	<tbody>
+		<tr>
+			<td>Sr.No</td>
+			<td>Name</td>
+			<td>Relation</td>
+			<td>Age</td>
+		</tr>
+		<tr>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+		</tr>
+		<tr>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+		</tr>
+	</tbody>
+</table>
+	
+	
+<p>10. List of Amenities:</p>
+<table style="width: 100%;">
+	<tbody>
+		<tr>
+			<td>Sr.No</td>
+			<td>Name</td>
+			<td>No. of Items</td>
+		</tr>
+		<tr>
+			<td></td>
+			<td></td>
+			<td></td>
+		</tr>
+		<tr>
+			<td></td>
+			<td></td>
+			<td></td>
+		</tr>
+	</tbody>
+</table>
+	
+<p>IN CASE THE LEAVE & LICENSE AGREEMENT IS EXTENDED THEN THE LICENSEE SHALL PAY THE COMMISSION/BROKERAGE TO THE ESTATE AGENT_____________________________________  </p>                                                                                                                             
+<p>IN WITHNESS WHEREOF THE PARTIES HERETO HAVE EXECUTED THIS AGREEMENT IN THE MNNER HEREINAFTER APPEARING ON TH DAY AND THE YEAR FIRST HEREIN ABOVE WRI</p>
+	<p><b>Within named 'LICENSOR'</b></p>
+<p><b>Mr./Mrs._____________________________________   </b></p>
+<p>In the presence of ………</p>
+<p>1.</p>
+	<p>2.</p>
+
+<p><b>SIGNED AND DEVELOPED by the</b></p>
+<p><b>Within named 'LICENSEE'</b></p>
+<p><b>Mr./Mrs._____________________________________   </b></p>
+	<p>In the presence of ………</p>
+	<p>1.</p>
+	<p>2.</p>
+	
+
+
+	
+</body>
+</html>
