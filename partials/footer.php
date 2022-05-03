a890a59b8ebefdd56753f207ef619497b0bced5d
whynaidu <naiduvedant@gmail.com>
2022-05-03 10:17:19 +0530

    Push

--- partials/footer.php
+++ partials/footer.php
@@ -1,6 +1,5 @@
 <footer class="footer">
-  <div class="d-sm-flex justify-content-center justify-content-sm-between">
-    <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">TECTIGNIS IT SOLUTIONS PVT. LTD.</span>
-    <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Copyright © 2021. All rights reserved.</span>
+  <div class="d-sm-flex justify-content-center">
+    PRODUCT BY&nbsp;<a href="https://tectignis.in/"><span class="text-muted text-center text-sm-left d-block d-sm-inline-block">TECTIGNIS IT SOLUTIONS.</span></a>
   </div>
 </footer>
